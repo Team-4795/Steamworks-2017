09c5a263d2dc976d45e03e2eb4bee38435206456
Blackjak34 <castleberrysam@gmail.com>
2017-02-07 14:13:34 -0500

    ZMQ subscriber listens on topic 'B'

--- src/org/usfirst/frc/team4795/robot/Robot.java
+++ src/org/usfirst/frc/team4795/robot/Robot.java
@@ -6,48 +6,54 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import java.nio.charset.Charset;
+
 import org.zeromq.*;
 
 public class Robot extends IterativeRobot {
     
-	ZMQ.Context context;
-	ZMQ.Socket subscriber;
+	private ZMQ.Context context;
+	private ZMQ.Socket subscriber;
 	
 	@Override
 	public void robotInit() {
 	    context = ZMQ.context(1);
 	    subscriber = context.socket(ZMQ.SUB);
 	    subscriber.bind("tcp://*:5808");
-	    subscriber.subscribe(new byte[]{});
+	    subscriber.subscribe("B".getBytes());
 	    subscriber.setReceiveTimeOut(10);
 	}
 	
 	@Override
+    public void teleopInit() {}
+	
+	@Override
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+        
+        // read and discard the topic
+        subscriber.recvStr(Charset.defaultCharset());
+        
+        String angle = subscriber.recvStr(Charset.defaultCharset());
+        if(!angle.equals("")) {
+            SmartDashboard.putString("Angle", angle);
+        }
+    }
+    
+	@Override
+    public void autonomousInit() {}
+    
+	@Override
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+    
+    @Override
 	public void disabledInit() {}
 
 	@Override
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
-	}
-	
-	@Override
-	public void autonomousInit() {}
-	
-	@Override
-	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	@Override
-	public void teleopInit() {}
-	
-	@Override
-	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
-		
-		byte[] angle = subscriber.recv();
-		if(angle != null)
-		    SmartDashboard.putString("Angle", angle.toString());
 	}
 	
 	@Override
