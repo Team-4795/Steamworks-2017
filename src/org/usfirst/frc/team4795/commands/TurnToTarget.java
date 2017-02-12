0bda99b2c25663ddfaed3256c5836955a79a367f
Amr Moussa <amrmoussa100@gmail.com>
2017-02-12 00:58:30 -0500

    Target Turning Command

---
+++ src/org/usfirst/frc/team4795/commands/TurnToTarget.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team4795.commands;
+
+import org.usfirst.frc.team4795.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class TurnToTarget extends Command {
+	private final double angle;
+
+	public TurnToTarget(double angle) {
+		this.angle = angle;
+	}
+
+	protected void initialize() {
+		Robot.drivetrain.rotateDegrees(angle, 10, 10, 10,0);
+	}
+
+	protected void execute() {
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	protected void end() {
+		
+	}
+
+	protected void interrupted() {
+		end();
+	}
+
+}
