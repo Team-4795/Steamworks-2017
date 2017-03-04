b7f9b594c4550fec7e2c69f7f1fc4d090a682fb5
Charles Zhang <charleszhang99@yahoo.com>
2017-03-03 21:20:36 -0500

    Change robot mapping

--- src/org/usfirst/frc/team4795/robot/RobotMap.java
+++ src/org/usfirst/frc/team4795/robot/RobotMap.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team4795.robot;
 
 
-public enum RobotMap 
+public enum RobotMap
 {
     // Motor mappings
     // PWM motor controllers
@@ -19,21 +19,21 @@ public enum RobotMap
     RIGHT_JOY(1),
     MANIPULATOR(2),
     // Left joystick button mappings
-    L_REVERSE_CONTROLS(6),
+    L_REVERSE_CONTROLS(3),
     L_INTAKE_IN(4),
     //L_INTAKE_OUT(6),
-    L_CLIMB_UP(3),
-    L_CLIMB_DOWN(5),
+    L_CLIMB_UP(5),
+    L_CLIMB_DOWN(6),
     // Right joystick button mappings
     //R_OVERRIDE(1), // override the manipulator controls
     //R_AUTO_SHOOT(1),
     R_AGITATOR_CW(5),
-    R_AGITATOR_CCW(3),
+    R_AGITATOR_CCW(6),
     R_SPIN_SHOOTER(1),
     // Manipulator button mappings
-    M_INTAKE_IN(9),
+    M_INTAKE_IN(1),
     M_INTAKE_OUT(10);
-    
+
     public final int value;
 
     RobotMap(int value) {
